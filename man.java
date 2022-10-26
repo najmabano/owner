b6e45690c85e545c71cd0469d417ee82ad64a823
najmabano <78802728+najmabano@users.noreply.github.com>
2022-10-26 14:07:26 +0530

    Update man.java

---
+++ man.java
@@ -0,0 +1,19 @@
+public class Datatype
+{
+ public static void main(String args[])
+  {
+   int var1;
+   float var2;
+   double var3;
+   long var4;
+   boolean var5;
+   string var6;
+   System.out.println("the primitive datatypes are");
+   System.out.println("var1=",+var1);
+   System.out.println("var2=",+var2);
+   System.out.println("var3=",+var3);
+   System.out.println("var4=",+var4);
+   System.out.println("var5=",+var5);
+   System.out.println("var6=",+var6);
+  }
+}
